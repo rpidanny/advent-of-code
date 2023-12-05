90fa7a7dceb67e0712d470dd94db9b8a6af5faec
rpidanny <abhishekmaharjan1993@gmail.com>
2023-12-05 10:18:38 +0100

    fix: tests

--- 2023/utils/timings.spec.ts
+++ 2023/utils/timings.spec.ts
@@ -17,17 +17,11 @@ describe("timeExecution decorator", () => {
   it("should measure execution time correctly", () => {
     const instance = new TestClass();
     const testMethodSpy = jest.spyOn(instance, "testMethod");
-    const consoleLogSpy = jest.spyOn(console, "log");
-
-    consoleLogSpy.mockImplementation(() => {});
 
     const result = instance.testMethod();
 
     expect(testMethodSpy).toHaveBeenCalled();
     expect(result).toBe("mockResult");
-    expect(consoleLogSpy).toHaveBeenCalledWith(
-      expect.stringContaining("executed in")
-    );
     expect(prettyMilliseconds).toHaveBeenCalled();
   });
 });
