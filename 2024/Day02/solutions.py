0553dd4bb8ea3f2825f93c6d0ed1161db7809161
Abhishek Maharjan <abhishekmaharjan1993@gmail.com>
2024-12-02 07:05:58 +0100

    feat(2024): day 2 - red-nosed reports (#12)
    * feat(2024): day 2 - red-nosed reports
    
    * feat(2024): day 2 - red-nosed reports

--- 2024/Day02/solutions.py
+++ 2024/Day02/solutions.py
@@ -1,6 +1,11 @@
+from red_nosed_reports import RedNosedReports
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    red_nosed_reports = RedNosedReports(inputs)
+    return red_nosed_reports.get_num_of_safe_reports()
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    red_nosed_reports = RedNosedReports(inputs)
+    return red_nosed_reports.get_num_of_safe_reports_with_removal()
