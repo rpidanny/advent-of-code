74ecd30e1201e7c22f823f20499979f8d22fcfd5
rpidanny <abhishekmaharjan1993@gmail.com>
2024-12-03 07:00:13 +0100

    feat(2024): day 3 - mull it over

--- 2024/Day03/solutions.py
+++ 2024/Day03/solutions.py
@@ -1,6 +1,26 @@
+import re
+
+
+def evaluate_instruction(instruction: str) -> int:
+    return eval(re.sub(r"[^\d,]", "", instruction).replace(",", "*"))
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    return sum(
+        evaluate_instruction(inst)
+        for line in inputs
+        for inst in re.findall(r"mul\(\d{1,3},\d{1,3}\)", line)
+    )
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    result, enabled = 0, True
+    for line in inputs:
+        for inst in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line):
+            if inst == "do()":
+                enabled = True
+            elif inst == "don't()":
+                enabled = False
+            elif enabled:
+                result += evaluate_instruction(inst)
+    return result
