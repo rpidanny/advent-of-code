261bd9da111791c63653b9cdf582f783b6b6e29a
rpidanny <abhishekmaharjan1993@gmail.com>
2024-12-06 07:41:27 +0100

    feat(2024): day 5 - Printer Queue

--- 2024/Day05/solutions.py
+++ 2024/Day05/solutions.py
@@ -1,6 +1,14 @@
+from printer_queue import PrinterQueue
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    pq = PrinterQueue(inputs)
+    correct_updates = pq.get_correct_updates()
+    return pq.get_sum_of_median(correct_updates)
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    pq = PrinterQueue(inputs)
+    incorrect_updates = pq.get_incorrect_updates()
+    fixed_updates = list(map(pq.fix_incorrect_update, incorrect_updates))
+    return pq.get_sum_of_median(fixed_updates)
