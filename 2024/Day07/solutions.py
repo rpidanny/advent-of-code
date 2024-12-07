05d33ce1e78378472e8a381884a171763e4580b2
Abhishek Maharjan <abhishekmaharjan1993@gmail.com>
2024-12-07 11:36:12 +0100

    feat(2024): day 7 - Bridge Repair (#16)

--- 2024/Day07/solutions.py
+++ 2024/Day07/solutions.py
@@ -1,6 +1,11 @@
+from bridge_repair import BridgeRepair
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    br = BridgeRepair(inputs)
+    return br.get_total_calibration(False)
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    br = BridgeRepair(inputs)
+    return br.get_total_calibration(True)
--- 2024/Day07/test_solutions.py
+++ 2024/Day07/test_solutions.py
@@ -13,18 +13,15 @@ input_test = get_inputs(f"{current_dir}/input_test.txt")
 
 class TestPart1:
     def test_with_test_data(self):
-        assert part1(input_test) == 1
+        assert part1(input_test) == 3749
 
-    @pytest.mark.skip(reason="not implemented")
     def test_with_real_data(self):
-        assert part1(input) == 1
+        assert part1(input) == 2664460013123
 
 
 class TestPart2:
-    @pytest.mark.skip(reason="not implemented")
     def test_with_test_data(self):
-        assert part2(input_test) == 2
+        assert part2(input_test) == 11387
 
-    @pytest.mark.skip(reason="not implemented")
     def test_with_real_data(self):
-        assert part2(input) == 2
+        assert part2(input) == 426214131924213
