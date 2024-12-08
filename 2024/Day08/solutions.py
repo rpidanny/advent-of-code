cbb6ab721746f714591a3075aa710cee226730e0
rpidanny <abhishekmaharjan1993@gmail.com>
2024-12-08 07:47:13 +0100

    feat(2024): day 8 - Resonant Collinearity

--- 2024/Day08/solutions.py
+++ 2024/Day08/solutions.py
@@ -1,6 +1,11 @@
+from resonant_collinearity import ResonantCollinearity
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    rc = ResonantCollinearity(inputs)
+    return rc.count_antinode()
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    rc = ResonantCollinearity(inputs)
+    return rc.count_antinode_with_harmonics()
