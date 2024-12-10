49ad1f97d3a609579fe78aef36145ec9e0a1cd83
rpidanny <abhishekmaharjan1993@gmail.com>
2024-12-10 06:38:08 +0100

    feat(2024): day 10 - Hook It

--- 2024/utils/directions.py
+++ 2024/utils/directions.py
@@ -1,1 +1,21 @@
-ALL_DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+UP = (0, 1)
+DOWN = (0, -1)
+RIGHT = (1, 0)
+LEFT = (-1, 0)
+UP_RIGHT = (1, 1)
+UP_LEFT = (-1, 1)
+DOWN_RIGHT = (1, -1)
+DOWN_LEFT = (-1, -1)
+
+BASIC_DIRECTIONS = [UP, DOWN, RIGHT, LEFT]
+
+ALL_DIRECTIONS = [
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+    UP_RIGHT,
+    UP_LEFT,
+    DOWN_RIGHT,
+    DOWN_LEFT,
+]
