c3e76aea11272bfe38ec27fb5f3dab9d7e964f52
rpidanny <abhishekmaharjan1993@gmail.com>
2024-12-10 07:55:31 +0100

    feat(2024): add CPU and Memory usage profiling

--- 2024/Day01/main.py
+++ 2024/Day01/main.py
@@ -6,7 +6,7 @@ sys.path.append(".")
 from solutions import part1, part2
 
 from utils.inputs import get_inputs
-from utils.timings import profile_run
+from utils.profiling import profile_run
 
 if __name__ == "__main__":
     input_path = f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"
