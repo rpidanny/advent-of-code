dbf4fad58cb300fca3d6f885e634ffad2463ff4c
rpidanny <abhishekmaharjan1993@gmail.com>
2024-12-11 19:01:10 +0100

    feat(2024): day 11 - Plutonian Pebbles

--- 2024/Day11/solutions.py
+++ 2024/Day11/solutions.py
@@ -1,6 +1,11 @@
+from plutonian_pebbles import PlutonianPebbles
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    pp = PlutonianPebbles(inputs)
+    return pp.simulate(25)
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    pp = PlutonianPebbles(inputs)
+    return pp.simulate(75)
