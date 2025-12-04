7526059e281938801e644385e1d52f58f867b0d5
rpidanny <abhishekmaharjan1993@gmail.com>
2025-12-04 06:42:13 +0100

    feat(2025): day 4 printing department

--- 2025/Day04/solutions.py
+++ 2025/Day04/solutions.py
@@ -1,6 +1,50 @@
+from utils import directions
+
+
 def part1(inputs: list[str]) -> int:
-    return 1
+    count, m, n = 0, len(inputs), len(inputs[0])
+
+    def is_valid_paper_roll(r: int, c: int) -> bool:
+        adj_count = 0
+        for dr, dc in directions.ALL_DIRECTIONS:
+            nr, nc = dr + r, dc + c
+            if 0 <= nr < m and 0 <= nc < n and inputs[nr][nc] == "@":
+                adj_count += 1
+                if adj_count >= 4:
+                    return False
+        return True
+
+    for r in range(m):
+        for c in range(n):
+            if inputs[r][c] == "@" and is_valid_paper_roll(r, c):
+                count += 1
+
+    return count
 
 
 def part2(inputs: list[str]) -> int:
-    return 2
+    grid = list(map(list, inputs))
+    count, m, n = 0, len(grid), len(grid[0])
+
+    def is_valid_paper_roll(r: int, c: int) -> bool:
+        adj_count = 0
+        for dr, dc in directions.ALL_DIRECTIONS:
+            nr, nc = dr + r, dc + c
+            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == "@":
+                adj_count += 1
+                if adj_count >= 4:
+                    return False
+        return True
+
+    while True:
+        remove_count = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == "@" and is_valid_paper_roll(r, c):
+                    grid[r][c] = "."
+                    remove_count += 1
+        if remove_count == 0:
+            break
+        count += remove_count
+
+    return count
