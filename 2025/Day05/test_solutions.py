c07dd97efb664dec86824652375b25a7dd50b201
rpidanny <abhishekmaharjan1993@gmail.com>
2025-12-05 07:14:47 +0100

    feat(2025): day 5 cafeteria

--- 2025/Day05/test_solutions.py
+++ 2025/Day05/test_solutions.py
@@ -1,6 +1,5 @@
 import os
 
-import pytest
 from solutions import part1, part2
 
 from utils.inputs import get_inputs
@@ -13,18 +12,15 @@ input_test = get_inputs(f"{current_dir}/input_test.txt")
 
 class TestPart1:
     def test_with_test_data(self):
-        assert part1(input_test) == 1
+        assert part1(input_test) == 3
 
-    @pytest.mark.skip(reason="not implemented")
     def test_with_real_data(self):
-        assert part1(input) == 1
+        assert part1(input) == 885
 
 
 class TestPart2:
-    @pytest.mark.skip(reason="not implemented")
     def test_with_test_data(self):
-        assert part2(input_test) == 2
+        assert part2(input_test) == 14
 
-    @pytest.mark.skip(reason="not implemented")
     def test_with_real_data(self):
-        assert part2(input) == 2
+        assert part2(input) == 348115621205535
