dc9738b115d8cc7d2de3feaeaef4ed70791677c3
Abhishek Maharjan <abhishekmaharjan1993@gmail.com>
2025-12-06 10:11:30 +0100

    feat(2025): day 6 trash compactor (#30)
    * feat(2025): day 6 trash compactor
    
    * chore: update vscode settings

--- 2025/Day06/solutions.py
+++ 2025/Day06/solutions.py
@@ -1,6 +1,43 @@
-def part1(inputs: list[str]) -> int:
-    return 1
+from helper import parseInput
 
 
-def part2(inputs: list[str]) -> int:
-    return 2
+def part1(lines: list[str]) -> int:
+    columns, total = parseInput(lines), 0
+
+    for col in columns:
+        operation = col[-1][0]
+        res = 0 if operation == "+" else 1
+
+        for r in range(0, len(col) - 1):
+            num = int(col[r])
+            if operation == "+":
+                res += num
+            else:
+                res *= num
+        total += res
+
+    return total
+
+
+def part2(lines: list[str]) -> int:
+    columns, total = parseInput(lines), 0
+
+    for col in columns:
+        operation = col[-1][0]
+        res = 0 if operation == "+" else 1
+
+        # go over the digit column to create the number and perform operation
+        for digit_index in range(len(col[0])):
+            num = []
+            # ignore the last row since it's for the operator
+            for r in range(len(col) - 1):
+                if col[r][digit_index] != " ":
+                    num.append(col[r][digit_index])
+
+            if operation == "+":
+                res += int("".join(num))
+            else:
+                res *= int("".join(num))
+
+        total += res
+    return total
