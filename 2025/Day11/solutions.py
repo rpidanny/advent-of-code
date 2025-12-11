d4fba6b8a43bbc1bf418188f4636d63b158d3c1f
rpidanny <abhishekmaharjan1993@gmail.com>
2025-12-11 17:09:00 +0100

    feat(2025): day 11 reactor

--- 2025/Day11/solutions.py
+++ 2025/Day11/solutions.py
@@ -1,6 +1,42 @@
-def part1(inputs: list[str]) -> int:
-    return 1
+from collections import defaultdict
+from functools import lru_cache
 
 
-def part2(inputs: list[str]) -> int:
-    return 2
+def parseInput(lines: list[str]):
+    adjList = defaultdict(list)
+    for line in lines:
+        src, dsts = line.split(": ")
+        adjList[src] = dsts.split(" ")
+    return adjList
+
+
+def part1(lines: list[str]) -> int:
+    adjList = parseInput(lines)
+
+    @lru_cache()
+    def dfs(node: str) -> int:
+        if node == "out":
+            return 1
+        count = 0
+        for dst in adjList[node]:
+            count += dfs(dst)
+        return count
+
+    return dfs("you")
+
+
+def part2(lines: list[str]) -> int:
+    adjList = parseInput(lines)
+
+    @lru_cache(maxsize=None)
+    def dfs(node: str, visited_dac: bool, visited_fft: bool) -> int:
+        if node == "out":
+            return int(visited_dac and visited_fft)
+
+        count = 0
+        for dst in adjList[node]:
+            count += dfs(dst, visited_dac or dst == "dac", visited_fft or dst == "fft")
+
+        return count
+
+    return dfs("svr", False, False)
--- 2025/Day11/test_solutions.py
+++ 2025/Day11/test_solutions.py
@@ -1,6 +1,5 @@
 import os
 
-import pytest
 from solutions import part1, part2
 
 from utils.inputs import get_inputs
@@ -9,22 +8,20 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 
 input = get_inputs(f"{current_dir}/input.txt")
 input_test = get_inputs(f"{current_dir}/input_test.txt")
+input_test_2 = get_inputs(f"{current_dir}/input_test_2.txt")
 
 
 class TestPart1:
     def test_with_test_data(self):
-        assert part1(input_test) == 1
+        assert part1(input_test) == 5
 
-    @pytest.mark.skip(reason="not implemented")
     def test_with_real_data(self):
-        assert part1(input) == 1
+        assert part1(input) == 466
 
 
 class TestPart2:
-    @pytest.mark.skip(reason="not implemented")
     def test_with_test_data(self):
-        assert part2(input_test) == 2
+        assert part2(input_test_2) == 2
 
-    @pytest.mark.skip(reason="not implemented")
     def test_with_real_data(self):
-        assert part2(input) == 2
+        assert part2(input) == 549705036748518
